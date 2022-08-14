421123f1d63bc3a97742f3d711e9fcaac6ae865f
Mathis Müller <107694673+mathismueller235@users.noreply.github.com>
2022-08-14 16:56:50 +0200

    Update swagger-initializer.js

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -2,8 +2,8 @@ window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
-  window.ui = SwaggerUIBundle({
-    //url: "https://petstore.swagger.io/v2/swagger.json",
+  const ui = SwaggerUIBundle({
+    url: "swagger.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
